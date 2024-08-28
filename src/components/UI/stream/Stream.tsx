028771753cd363dccd58a6a2193a8d90d4572254
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-08-27 23:48:39 -0300

    asd

--- src/components/UI/stream/Stream.tsx
+++ src/components/UI/stream/Stream.tsx
@@ -1,7 +1,7 @@
 const Stream = () => {
   return (
     <div className="w-11/12 flex justify-center mt-4 mb-4">
-      <div className="w-4/5   h-64 rounded-xl  overflow-hidden ">
+      <div className="w-4/5 h-64 rounded-xl  overflow-hidden ">
         <iframe
           src="https://player.kick.com/c0ker"
           className="w-full h-full "
