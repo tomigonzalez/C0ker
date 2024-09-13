841eef766fbca0f3de3bd35e9cf1ee3cb1c4e46a
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-09-12 23:59:16 -0300

    actualizacion

--- src/components/sobreMi/SobreMi.tsx
+++ src/components/sobreMi/SobreMi.tsx
@@ -4,14 +4,14 @@ const SobreMi = () => {
       <h2 className="text-xs mt-2 mb-6">Sobre Mi</h2>
       <div className="w-11/12 flex flex-col gap-4 ">
         <div className="flex xl:flex-row lg:flex-row items-center text-center flex-col w-full xl:gap-14 lg:gap-14 gap-6">
-          <div className="w-[600px] xl:w-11/12 lg:w-11/12">
+          <div className="w-[80%] xl:w-11/12 lg:w-11/12">
             <img
               src="assets/c0kersobremi2.png"
               className="rounded-2xl w-full shadow-lg"
               alt="C0ker"
             />
           </div>
-          <div className="  w-full text-xs content-center">
+          <div className="  w-full xl:text-xs lg:text-xs text-[0.5rem] content-center">
             <p className="">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
               similique iusto, unde nam, aperiam quod ratione minima omnis,
@@ -21,7 +21,7 @@ const SobreMi = () => {
           </div>
         </div>
         <div className="flex xl:flex-row lg:flex-row items-center text-center  flex-col-reverse  w-full xl:gap-14 lg:gap-14 gap-6">
-          <div className="w-full text-xs content-center">
+          <div className="w-full xl:text-xs lg:text-xs text-[0.5rem] content-center">
             <p className=" ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
               similique iusto, unde nam, aperiam quod ratione minima omnis,
@@ -29,7 +29,7 @@ const SobreMi = () => {
               eveniet rem modi!
             </p>
           </div>
-          <div className="w-[600px] xl:w-11/12 lg:w-11/12">
+          <div className="w-[80%] xl:w-11/12 lg:w-11/12">
             <img
               src="assets/c0kersobremi.png"
               className="rounded-2xl w-full shadow-lg"
@@ -38,15 +38,15 @@ const SobreMi = () => {
           </div>
         </div>
         <div className="flex xl:flex-row lg:flex-row items-center text-center flex-col  w-full xl:gap-14 lg:gap-14 gap-6">
-          <div className="w-[600px] xl:w-full lg:w-full ">
+          <div className="w-[80%] xl:w-full lg:w-full ">
             <img
               src="assets/c0kersobremi1.png"
               className="rounded-2xl w-full h-full shadow-lg "
               alt="C0ker"
             />
           </div>
-          <div className="w-full text-xs content-center">
-            <p className=" h-1/2">
+          <div className="w-full  xl:text-xs lg:text-xs text-[0.5rem] content-center">
+            <p className=" ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
               similique iusto, unde nam, aperiam quod ratione minima omnis,
               vitae
