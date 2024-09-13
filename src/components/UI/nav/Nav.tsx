28b1706b7a8069b96803251086fe94bc6211f519
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-09-13 00:56:50 -0300

    actualizacio nav

--- src/components/UI/nav/Nav.tsx
+++ src/components/UI/nav/Nav.tsx
@@ -2,14 +2,39 @@ import { HiMenuAlt3, HiX } from "react-icons/hi";
 import CircleRec from "./circleRec/CircleRec";
 import ModalMenu from "./modalMenu/ModalMenu";
 import "../nav/atroposFichas/AtroposStyle.css";
+import { useEffect, useState } from "react";
 type Props = {
   isOpen: boolean;
   toggleMenu: () => void;
 };
 const Nav = (props: Props) => {
+  const [showNav, setShowNav] = useState(true); // Estado para controlar la visibilidad del nav
+  const [lastScrollY, setLastScrollY] = useState(0); // Para almacenar la última posición de scroll
+
+  // Función que controla la visibilidad del nav en función del scroll
+  const controlNavbar = () => {
+    if (window.scrollY > lastScrollY) {
+      setShowNav(false); // Ocultar el nav cuando hacemos scroll hacia abajo
+    } else {
+      setShowNav(true); // Mostrar el nav cuando hacemos scroll hacia arriba
+    }
+    setLastScrollY(window.scrollY); // Actualizar la posición del scroll
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar); // Limpiar el listener
+    };
+  }, [lastScrollY]);
+
   return (
     <>
-      <nav className="w-full h-12 fixed z-50 flex flex-col items-center bg-cover bg-center bg-gray-to-transparent">
+      <nav
+        className={`w-full h-12 fixed z-50 flex flex-col items-center bg-cover bg-center bg-gray-to-transparent transition-transform duration-300 ${
+          showNav ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
         <div className="w-11/12  h-full flex flex-row justify-between items-center">
           <div className="w-1/4 flex xl:text-[1rem] sm:text-[0.9rem] text-[0.6rem]">
             <div className="w-3.5">
